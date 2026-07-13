13e5a01135f9536193f2f0f9c18083aeaf288de5
Elijah Chong <dissaryy@gmail.com>
2026-07-13 14:33:21 +0800

    firebase configuration & vercel

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,6 +1,9 @@
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
-const serviceAccount = require("./serviceAccountKey.json");
+
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
+  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+  : require("./serviceAccountKey.json");
 
 const app = initializeApp({
   credential: cert(serviceAccount),
